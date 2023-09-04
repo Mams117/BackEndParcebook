d3d599e8f699ba57f5b8983c947656a3e6a06dbf
Mams117 <adsoadso17@gmail.com>
2023-09-04 17:02:56 -0500

    backEnd

---
+++ index.js
@@ -0,0 +1,37 @@
+//librerias necesarias para la aplicacion
+const express = require("express"); //para programar los verbos HTTP
+const cors = require("cors");
+
+const app = express();
+const puerto = 3900;
+//middleware :servicios comunes para la aplicacion ayuda en la construccion
+
+app.use(cors()); //acepta llamadas de url no conocidas
+app.use(express.json()); //permite intercambiar en formato JSON
+
+//rutas
+
+app.get("/", (req, res) => {
+  //console.log("ruta base");
+  return res.status(200).send(`<h1> bienvenido al backend de parcebook</h1>
+  <ul>
+  <li>javaScript</li>
+  <li>Php</li>
+  <li>Phyton</li>
+  </ul>
+  `);
+});
+
+app.get("/clientes", (req, res) => {
+  return res.status(200).send({
+    id: 230,
+    nombre: "mario",
+    estado: true,
+    edad: 20,
+  });
+});
+
+//activamos el servidor del backEnd
+app.listen(puerto, () => {
+  console.log("corriendo en el puerto ", puerto);
+});
---
+++ model/Conexion.js
@@ -0,0 +1,1 @@
+//clase donde instanciamos mongo
